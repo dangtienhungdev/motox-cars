472f9e9a83bdac6e45ba8029ab3159a01050cc1b
dangtienhung <danghung02042003@gmail.com>
2024-07-26 22:32:54 +0700

    update code

--- client-v2/vite.config.ts
+++ client-v2/vite.config.ts
@@ -1,6 +1,6 @@
-import { createHtmlPlugin } from 'vite-plugin-html';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +19,13 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': '/src',
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: './index.html',
+			},
 		},
 	},
 });
