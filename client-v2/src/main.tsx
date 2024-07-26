3f239aeaba1fa4651190349ea13ef369b0c07ee5
dangtienhung <danghung02042003@gmail.com>
2024-07-26 21:38:38 +0700

    update code

--- client-v2/src/main.tsx
+++ client-v2/src/main.tsx
@@ -1,10 +1,20 @@
 import './index.css';
+import './styles/all.min.css';
+import './styles/animate.css';
+import './styles/bootstrap.min.css';
+import './styles/datepickerboot.css';
+import './styles/magnific-popup.css';
+import './styles/main.css';
+import './styles/meanmenu.css';
+import './styles/nice-select.css';
+import './styles/swiper-bundle.min.css';
 
 import { ConfigProvider } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import theme from './theme-antd.tsx';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<ConfigProvider theme={theme}>
--- client-v2/vite.config.ts
+++ client-v2/vite.config.ts
@@ -1,9 +1,20 @@
-import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	base: process.env.VITE_BASE_URL,
+	plugins: [
+		react(),
+		createHtmlPlugin({
+			inject: {
+				data: {
+					baseUrl: process.env.VITE_BASE_URL,
+				},
+			},
+		}),
+	],
 	// alias config
 	resolve: {
 		alias: {
