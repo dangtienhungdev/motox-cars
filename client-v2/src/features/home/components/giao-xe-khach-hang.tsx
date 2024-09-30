e4ed9c1e607d6adeb5ac03b168d5df0f91ba8700
dangtienhung <danghung02042003@gmail.com>
2024-09-30 20:59:27 +0700

    feat: update image

--- client-v2/src/features/home/components/giao-xe-khach-hang.tsx
+++ client-v2/src/features/home/components/giao-xe-khach-hang.tsx
@@ -2,216 +2,136 @@ const GiaoXeKhachHang = () => {
 	return (
 		<div className="gallery-section fix">
 			<div className="gallery-wrapper">
-				<div className="row g-4">
-					<div className="col-xxl-4 col-xl-5 col-lg-5">
-						<div className="row g-4">
-							{/* 1 */}
-							<div className="col-lg-6 col-md-6 col-sm-6">
-								<div className="row g-4">
-									<div className="col-md-12">
-										<div className="gallery-image">
-											<img
-												src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
-												alt="img"
-											/>
-											<div className="icon-box">
-												<a
-													href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
-													className="icon img-popup-2"
-												>
-													<i className="fa-solid fa-plus" />
-												</a>
-											</div>
-											<div className="mask" />
-											<div className="mask-second" />
-										</div>
-									</div>
-									<div className="col-md-12">
-										<div className="gallery-image">
-											<img
-												src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543199435_45eb9cd95952bab8f73a29ccd9058e6e_ta78yl.jpg"
-												alt="img"
-											/>
-											<div className="icon-box">
-												<a
-													href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543199435_45eb9cd95952bab8f73a29ccd9058e6e_ta78yl.jpg"
-													className="icon img-popup-2"
-												>
-													<i className="fa-solid fa-plus" />
-												</a>
-											</div>
-											<div className="mask" />
-											<div className="mask-second" />
-										</div>
-									</div>
-								</div>
-							</div>
-							{/* 2 */}
-							<div className="col-lg-6 col-md-6 col-sm-6">
-								<div className="row g-4">
-									<div className="col-md-12">
-										<div className="gallery-image">
-											<img
-												src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
-												alt="img"
-											/>
-											<div className="icon-box">
-												<a
-													href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
-													className="icon img-popup-2"
-												>
-													<i className="fa-solid fa-plus" />
-												</a>
-											</div>
-											<div className="mask" />
-											<div className="mask-second" />
-										</div>
-									</div>
-									<div className="col-md-12">
-										<div className="gallery-image">
-											<img
-												src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
-												alt="img"
-											/>
-											<div className="icon-box">
-												<a
-													href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
-													className="icon img-popup-2"
-												>
-													<i className="fa-solid fa-plus" />
-												</a>
-											</div>
-											<div className="mask" />
-											<div className="mask-second" />
-										</div>
-									</div>
-								</div>
-							</div>
+				<div className="gap-4 tw-grid tw-grid-cols-4 md:!tw-grid-cols-3 !tw-grid-cols-2">
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882358545554_b9e917a98f7abbe477f7e2229da8c50b_y42a6b.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882358545554_b9e917a98f7abbe477f7e2229da8c50b_y42a6b.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
 
-							{/* <div className="col-lg-6 col-md-6 col-sm-6">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
-											className="icon img-popup-2 style-two"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div> */}
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882360572336_4517f310a181b76c84c5dd623a763fec_vmlyeo.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882360572336_4517f310a181b76c84c5dd623a763fec_vmlyeo.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
 						</div>
+						<div className="mask" />
+						<div className="mask-second" />
 					</div>
-					<div className="col-xxl-6 col-xl-7 col-lg-7">
-						<div className="row g-4">
-							<div className="col-md-4 col-sm-6">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
-											className="icon img-popup-2"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div>
-							<div className="col-md-4 col-sm-6">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233137/tran-xuan-hieu/giao-xe/z5607543210942_05eb67a53991dcbc09009e91f84b8ab0_xrz0ul.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233137/tran-xuan-hieu/giao-xe/z5607543210942_05eb67a53991dcbc09009e91f84b8ab0_xrz0ul.jpg"
-											className="icon img-popup-2"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div>
-							<div className="col-md-4 col-sm-6">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
-											className="icon img-popup-2"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div>
-							<div className="col-md-4 col-sm-6">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233135/tran-xuan-hieu/giao-xe/z5607543170985_84838613bbb945e95194cf33f7126cbb_fcva8m.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233135/tran-xuan-hieu/giao-xe/z5607543170985_84838613bbb945e95194cf33f7126cbb_fcva8m.jpg"
-											className="icon img-popup-2"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div>
-							<div className="col-md-8">
-								<div className="gallery-image">
-									<img
-										src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233143/tran-xuan-hieu/giao-xe/z5607543410892_273dc9894db567e95117aaf0fe3ab510_uded1l.jpg"
-										alt="img"
-									/>
-									<div className="icon-box">
-										<a
-											href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233143/tran-xuan-hieu/giao-xe/z5607543410892_273dc9894db567e95117aaf0fe3ab510_uded1l.jpg"
-											className="icon img-popup-2"
-										>
-											<i className="fa-solid fa-plus" />
-										</a>
-									</div>
-									<div className="mask" />
-									<div className="mask-second" />
-								</div>
-							</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882358554637_8b62f735161fd00c38cafd131560ab33_raexgd.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704088/tran-xuan-hieu/giao-xe/z5882358554637_8b62f735161fd00c38cafd131560ab33_raexgd.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
 						</div>
+						<div className="mask" />
+						<div className="mask-second" />
 					</div>
-					<div className="col-xxl-2 col-xl-4 col-lg-4 col-md-6 col-sm-6">
-						<div className="gallery-image style-2">
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233135/tran-xuan-hieu/giao-xe/z5607543170985_84838613bbb945e95194cf33f7126cbb_fcva8m.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233135/tran-xuan-hieu/giao-xe/z5607543170985_84838613bbb945e95194cf33f7126cbb_fcva8m.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233137/tran-xuan-hieu/giao-xe/z5607543210942_05eb67a53991dcbc09009e91f84b8ab0_xrz0ul.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233137/tran-xuan-hieu/giao-xe/z5607543210942_05eb67a53991dcbc09009e91f84b8ab0_xrz0ul.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233145/tran-xuan-hieu/giao-xe/z5607543019458_f0419f1f1f2030571e34be5034e4996b_ko7tlq.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="col-md-12">
+						<div className="gallery-image">
 							<img
-								src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
+								src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
 								alt="img"
 							/>
 							<div className="icon-box">
 								<a
-									href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721232989/tran-xuan-hieu/giao-xe/1_kxj2k9.jpg"
-									className="icon img-popup-2 style-two"
+									href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543392783_754b92a67d0c64186dabc5e216829645_sxltwd.jpg"
+									className="icon img-popup-2"
 								>
 									<i className="fa-solid fa-plus" />
 								</a>
@@ -220,6 +140,90 @@ const GiaoXeKhachHang = () => {
 							<div className="mask-second" />
 						</div>
 					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543199435_45eb9cd95952bab8f73a29ccd9058e6e_ta78yl.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1721233140/tran-xuan-hieu/giao-xe/z5607543199435_45eb9cd95952bab8f73a29ccd9058e6e_ta78yl.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704577/tran-xuan-hieu/giao-xe/z5714943016742_c714a84030b64291033cba5e2e00d29f_i8opmy.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704577/tran-xuan-hieu/giao-xe/z5714943016742_c714a84030b64291033cba5e2e00d29f_i8opmy.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704577/tran-xuan-hieu/giao-xe/z5714943037183_a7433ad81663da0398e6e78ef8a6d4f2_lnsjwy.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704577/tran-xuan-hieu/giao-xe/z5714943037183_a7433ad81663da0398e6e78ef8a6d4f2_lnsjwy.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					<div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704667/tran-xuan-hieu/giao-xe/z5650311432672_5a85cf7573fb836beb01dbbcd532abb0_lt9nx5.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704667/tran-xuan-hieu/giao-xe/z5650311432672_5a85cf7573fb836beb01dbbcd532abb0_lt9nx5.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div>
+
+					{/* <div className="gallery-image">
+						<img
+							src="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704667/tran-xuan-hieu/giao-xe/z5650311432672_5a85cf7573fb836beb01dbbcd532abb0_lt9nx5.jpg"
+							alt="img"
+						/>
+						<div className="icon-box">
+							<a
+								href="https://res.cloudinary.com/dcwdrvxdg/image/upload/v1727704667/tran-xuan-hieu/giao-xe/z5650311432672_5a85cf7573fb836beb01dbbcd532abb0_lt9nx5.jpg"
+								className="icon img-popup-2"
+							>
+								<i className="fa-solid fa-plus" />
+							</a>
+						</div>
+						<div className="mask" />
+						<div className="mask-second" />
+					</div> */}
 				</div>
 			</div>
 		</div>
